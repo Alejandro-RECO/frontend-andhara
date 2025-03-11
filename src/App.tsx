3b6c6a8031c11d0d2f1ea1b1bd3572b5a7a6a43e
AND3SIL4 <afsilva6236@misena.edu.co>
2025-03-11 00:42:23 -0500

    test(tools): aaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
 const App = () => {
-
   return (
     <>
-     <h1 className="bg-red-400">Welcome Lubusi</h1>
+      <h1 className="bg-red-400">Welcome Lubusi</h1>
+      <p>Holaa</p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
